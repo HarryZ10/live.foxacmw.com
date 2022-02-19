9d69f8d2f2058064df89b84421a5a326d5c06e63
Harry <hzhu20@georgefox.edu>
2022-02-19 01:24:18 -0800

    Add more UI friendly

--- src/js/gitlab/team_5.jsx
+++ src/js/gitlab/team_5.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import useFetch from 'react-fetch-hook'
 import Card from 'react-bootstrap/Card'
 import { BallTriangle } from  'react-loader-spinner'
+import TeamModal from '../teamModal'
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 export default function TeamOne() {
+
+    const modalRef = useRef();
 
     const { isLoading, error, data}  = useFetch(
         `https://gitlab.com/api/v4/projects/33664488/repository/commits`,
@@ -19,8 +24,53 @@ export default function TeamOne() {
 
     return (
         <div>
+
+<TeamModal ref={modalRef}>
+                <div className="grid">
+                    <div className="whole-modal">
+                    </div>
+
+                    <div className="title-modal">
+                        <h1 id="modal-title">Team 5</h1>
+                    </div>
+
+                    <div className="content-in-modal">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Donec eget nunc eget nunc tincidunt aliquam.
+                            Sed euismod, urna eu tincidunt consectetur,
+                            nisi nisl tincidunt nisi, eget congue nisl nisi
+                            vitae nisl.
+                        </p>
+                    </div>
+
+                    <div className="footer-modal" style={{paddingTop: 20}}>
+
+                        <p>
+                            <p><span className="lead">Total Commits:</span> {data.length} </p>
+                            <p>
+                                <span className="lead">All Commits:</span>
+                                {/* scroll box */}
+                                <div id="scrollbox">
+                                    <SimpleBar style={{ maxHeight: 200, paddingTop: 10, paddingBottom: 10 }}>
+                                        {data.map(commit => (
+                                            <li key={commit.id}>
+                                                <a href={commit.web_url} target="_blank" rel="noopener noreferrer">
+                                                    "{commit.message}"
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </SimpleBar>
+                                </div>
+                            </p>
+                        </p>
+                    </div>
+                </div>
+
+            </TeamModal>
+            
             <Card id="team-card" style={{ width: '80%', marginBottom: '2rem' }}>
-            <Card.Body style={{ paddingTop: '1.2rem', paddingBottom: '1.2rem'}}>
+            <Card.Body onClick={() => modalRef.current.open()} style={{ paddingTop: '1.2rem', paddingBottom: '1.2rem'}}>
                 <Card.Title style={title}>Team 5</Card.Title>
                 <Card.Subtitle style={subtitle} className="mb-2 text-muted"># of Commits: {data.length}</Card.Subtitle>
                 <Card.Text style={subtitle}>
