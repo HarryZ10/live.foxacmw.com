62994291439603a20e88d0cc0032223ed4a58415
Harry <hzhu20@georgefox.edu>
2022-03-17 23:10:30 -0700

    ROll out resoruces

--- src/js/resources.jsx
+++ src/js/resources.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import discordIcon from "../svg/discord.svg";
 import devpostIcon from "../svg/devpost.svg";
+import formsIcon from "../svg/forms.svg";
 
 function Resources() {
   return (
@@ -15,8 +16,14 @@ function Resources() {
       </a>
       <a href="https://foxacmw.devpost.com" rel="noopener noreferrer" target="_blank">
         <div className="resource-2">
-          <img src={devpostIcon} alt="dev post icon" className="resource-icon" />
+          <img src={devpostIcon} alt="devpost icon" className="resource-icon" />
           Devpost
+        </div>
+      </a>
+      <a href="https://forms.gle/i7LdZwAkzMaysJ7H7" rel="noopener noreferrer" target="_blank">
+        <div className="resource-3">
+          <img src={formsIcon} alt="forms icon" className="resource-icon" />
+          Teams Check-In Form <small style={{ fontSize: '0.8rem'}}>(Due April 2nd)</small>
         </div>
       </a>
     </div>
