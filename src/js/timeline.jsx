43b6a05a4aed4a1dc594516081edf91e156720bd
Harry <harryzhu45@gmail.com>
2023-03-11 23:14:06 -0800

    Add timeline

--- src/js/timeline.jsx
+++ src/js/timeline.jsx
@@ -1,69 +1,69 @@
 import React from 'react';
 
 const timelineData = [
-    {
-        text: 'Doors open!',
-        date: 'Friday at 4pm',
-        category: {
-            tag: 'EHS 102',
-            color: '#4764f6'
-        },
-        description: 'Get excited and hyped for the Code Jam launch!',
-    },
-    {
-        text: 'Code Jam Kickoff',
-        date: 'Friday at 4pm',
-        category: {
-            tag: 'EHS 102',
-            color: '#4764f6'
-        },
-        description: 'Hear more about what to expect and learn for the Code Jam.',
-    },
-    {
-        text: 'Mentor x Team Networking',
-        date: 'Friday at 4:30pm',
-        category: {
-            tag: 'EHS Atrium',
-            color: '#00520c'
-        },
-        description: 'Bring on the team morale!',
-    },
-    {
-        text: 'Workshop: Introduction to Hosting',
-        date: 'Friday at 4:45pm (15 minutes)',
-        category: {
-            tag: 'EHS 102',
-            color: '#00520c'
-        }
-    },
-    {
-        text: 'Hacking begins!',
-        date: 'Friday at 5pm',
-        category: {
-            tag: 'code jam',
-            color: '#e17b77'
-        },
-        link: {
-            url: '#',
-            text: 'Team Check-In Form (by 9pm PT)'
-        }
-    },
-    {
-        text: 'Twilight ACM-W Office Hours',
-        date: 'TBA',
-        category: {
-            tag: 'help desk',
-            color: '#545ecc'
-        }
-    },
-        {
-        text: 'ACM-W Office Hours',
-        date: 'TBA',
-        category: {
-            tag: 'help desk',
-            color: '#545ecc'
-        },
-    },
+    // {
+    //     text: 'Doors open!',
+    //     date: 'Friday at 4pm',
+    //     category: {
+    //         tag: 'EHS 102',
+    //         color: '#4764f6'
+    //     },
+    //     description: 'Get excited and hyped for the Code Jam launch!',
+    // },
+    // {
+    //     text: 'Code Jam Kickoff',
+    //     date: 'Friday at 4pm',
+    //     category: {
+    //         tag: 'EHS 102',
+    //         color: '#4764f6'
+    //     },
+    //     description: 'Hear more about what to expect and learn for the Code Jam.',
+    // },
+    // {
+    //     text: 'Mentor x Team Networking',
+    //     date: 'Friday at 4:30pm',
+    //     category: {
+    //         tag: 'EHS Atrium',
+    //         color: '#00520c'
+    //     },
+    //     description: 'Bring on the team morale!',
+    // },
+    // {
+    //     text: 'Workshop: Introduction to Hosting',
+    //     date: 'Friday at 4:45pm (15 minutes)',
+    //     category: {
+    //         tag: 'EHS 102',
+    //         color: '#00520c'
+    //     }
+    // },
+    // {
+    //     text: 'Hacking begins!',
+    //     date: 'Friday at 5pm',
+    //     category: {
+    //         tag: 'code jam',
+    //         color: '#e17b77'
+    //     },
+    //     link: {
+    //         url: '#',
+    //         text: 'Team Check-In Form (by 9pm PT)'
+    //     }
+    // },
+    // {
+    //     text: 'Twilight ACM-W Office Hours',
+    //     date: 'TBA',
+    //     category: {
+    //         tag: 'help desk',
+    //         color: '#545ecc'
+    //     }
+    // },
+    //     {
+    //     text: 'ACM-W Office Hours',
+    //     date: 'TBA',
+    //     category: {
+    //         tag: 'help desk',
+    //         color: '#545ecc'
+    //     },
+    // },
     {
         text: 'Hacking Ends!',
         date: 'Sunday at 12:00am',
@@ -88,14 +88,13 @@ const timelineData = [
     {
         text: 'Celebratory Banquet',
         date: 'Sunday at 11:30am',
-        description: 'Dress to impress!',
         category: {
             tag: 'EHS Atrium',
             color: '#4764f6'
         }
     },
     {
-        text: 'Alumni SWE Panel',
+        text: 'Computer Science Alum Panel',
         date: 'Sunday at 1-1:45pm',
         category: {
             tag: 'EHS 102',
@@ -104,16 +103,25 @@ const timelineData = [
         description: 'Come meet all the working alumni & mentors, and other hackers that have helped put this on for you all!',
     },
     {
-        text: 'Annual Code Jam Showcase 2022',
-        date: 'Sunday at 2pm',
+        text: '3rd Annual Code Jam Showcase',
+        date: 'Sunday at 2pm (until 3:10pm)',
         category: {
             tag: 'EHS 102',
             color: '#4764f6'
         },
     },
     {
-        text: 'Award Ceremony & Team Awards',
+        text: 'Networking Break (ft. Green Tea and Lemonade)',
+        date: 'Sunday at 3:30pm',
+        category: {
+            tag: 'EHS Atrium',
+            color: '#4764f6'
+        },
+    },
+    {
+        text: '3rd Annual Award Ceremony',
         date: 'Sunday at 4pm',
+        description: 'Come see who wins the coveted ACM-W Code Jam 2022 awards, and invite your friends to come see the showcase!',
         category: {
             tag: 'EHS 102',
             color: '#4764f6'
